52352fd184789a364241f74813d5aa95a624738a
SirYummy <micah.bolen@gmail.com>
2018-01-24 09:18:34 -0600

    Implement changes based on code review. - Remove IIFE - Replace search limit dropdown with number input. - Confine `fetchLimit` and `searchText` to the scope of the form submit listener. - Make `fetchGifs` more reusable by adding search limit as an additional parameter. - Replace ternary operator in `fetchGifs` filter callback with a simple boolean expression. - Replace if-statement in `populateGrid` with a logical OR operator: `||`. - Replace `noResults` function with a more abstracted and reusable `displayNoResults` function.

--- app.js
+++ app.js
@@ -1,91 +1,82 @@
-(() => {
+const API_KEY = 'CZoppU1XxfCevaD9Ilrr9kQb0vXWC1su'
+const BASE_URL = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=`
 
-    const API_KEY = 'CZoppU1XxfCevaD9Ilrr9kQb0vXWC1su'
-    const BASE_URL = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=`
+const gifForm = document.querySelector('#gif-form')
+const gifGrid = document.querySelector('#gif-grid')
+const gifInput = gifForm.querySelector('#gif-search-input')
+const gifLimiter = gifForm.querySelector('#gif-search-limit')
 
-    const gifForm = document.querySelector('#gif-form')
-    const gifGrid = document.querySelector('#gif-grid')
-    const gifInput = gifForm.querySelector('#gif-search-input')
-    const gifLimiter = gifForm.querySelector('#gif-search-limit')
+gifInput.focus()
 
-    let fetchLimit = gifLimiter.value
-    let searchText = gifInput.value
+gifForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-    gifInput.focus()
+    const fetchLimit = gifLimiter.value
+    const searchText = gifInput.value
 
-    gifForm.addEventListener('submit', (e) => {
-        e.preventDefault()
+    removeChildren(gifGrid)
+
+    fetchGifs(searchText, fetchLimit)
+    .then(populateGrid)
+})
+
+const fetchGifs = (searchTerm, limit) => {
+    return fetch(`${BASE_URL}${searchTerm}`)
+    .then(res => res.json())
+    .then(data => data.data.filter((item, index) => {
+    return index < limit
+    }))
+}
+
+const populateGrid = (data = []) => {
     
-        fetchLimit = gifLimiter.value
-        searchText = gifInput.value
+    data.map((gif, gifIndex) => {
+        let gifInstance = appendGif(gif, gifIndex)
+        renderGif(gifInstance, gifIndex)
+    }).length || displayNoResults(gifGrid, '0 results found')
+   
+}
 
-        removeChildren(gifGrid)
+const displayNoResults = (element, message) => {
+    element.innerText = message
+}
 
-        fetchGifs(searchText)
-        .then(populateGrid)
-    })
+const appendGif = (gif, gifIndex) => {
+    
+    const gifContainer = document.createElement('figure')
+    const gifImage = document.createElement('img')
+    const gifCaption = document.createElement('figcaption')
 
-    const fetchGifs = (searchTerm) => {
-        return fetch(`${BASE_URL}${searchTerm}`)
-        .then(res => res.json())
-        .then(data => data.data.filter((item, index) => {
-        return index < fetchLimit ? item : ''
-        }))
-    }
+    gifContainer.appendChild(gifImage)
+    gifContainer.appendChild(gifCaption)
 
-    const populateGrid = (data) => {
-        if(data.length) {
-            data.map((gif, gifIndex) => {
-                let gifInstance = appendGif(gif, gifIndex)
-                renderGif(gifInstance, gifIndex)
-            })
-        } else {
-            noResults()
-        }
-    }
+    gifImage.setAttribute('src', gif.images.fixed_height.url)
+    gifCaption.innerText = gif.title
+    gifGrid.appendChild(gifContainer)
 
-    const noResults = () => {
-        gifGrid.innerText = '0 results found'
-    }
+    return gifContainer
+    
+}
 
-    const appendGif = (gif, gifIndex) => {
-        
-        const gifContainer = document.createElement('figure')
-        const gifImage = document.createElement('img')
-        const gifCaption = document.createElement('figcaption')
+const renderGif = (gifInstance, gifIndex) => {
+    const gifAnimation = 'fadeInUp'
 
-        gifContainer.appendChild(gifImage)
-        gifContainer.appendChild(gifCaption)
-
-        gifImage.setAttribute('src', gif.images.fixed_height.url)
-        gifCaption.innerText = gif.title
-        gifGrid.appendChild(gifContainer)
-
-        return gifContainer
-        
-    }
-
-    const renderGif = (gifInstance, gifIndex) => {
-        const gifAnimation = 'fadeInUp'
-
-        if(!gifIndex) {
+    if(!gifIndex) {
+        animate(gifInstance, gifAnimation)
+    } else {
+        setTimeout(() => {
             animate(gifInstance, gifAnimation)
-        } else {
-            setTimeout(() => {
-                animate(gifInstance, gifAnimation)
-            }, 500 * gifIndex)
-        }
+        }, 500 * gifIndex)
     }
+}
 
-    const animate = (animateMe, transitionType) => {
-        animateMe.style.display = 'block'
-        animateMe.setAttribute('class', `animated ${transitionType}`)
+const animate = (animateMe, transitionType) => {
+    animateMe.style.display = 'block'
+    animateMe.setAttribute('class', `animated ${transitionType}`)
+}
+
+const removeChildren = (parent) => {
+    while(parent.hasChildNodes()) {
+        parent.removeChild(parent.lastChild)
     }
-
-    const removeChildren = (parent) => {
-        while(parent.hasChildNodes()) {
-            parent.removeChild(parent.lastChild)
-        }
-    }
-
-})()
+}
